e7ecca9354a16d02ceaf87bda14ac695bccddb9f
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-14 02:22:38 +0100

    links added to main navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { RouterProvider,createBrowserRouter } from "react-router-dom";
-
-import HomePage from './pages/Home';
-import EventsPage from './pages/Events.js';
-import EventDetailPage from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
-import EditEventPage from './pages/EditEvent';
-import RootLayout from './pages/Root';
-
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events.js";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  {path : '/',
-  element : <RootLayout />,
-  children: [
-    {path: '', element: <HomePage/>},
-    {path: 'events', element: <EventsPage/>},
-    {path: 'events/:eventId', element: <EventDetailPage/>},
-    {path: 'newEvent', element: <NewEventPage/>},
-    {path: 'editEvent', element: <EditEventPage/>},
-    
-  ]
-}
-])
-
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index:true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
