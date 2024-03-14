7de23cfd82090158bb63c7f9afad8f75930fc092
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-14 05:10:44 +0100

    Error handling with json method from router

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -4,15 +4,17 @@ import PageContent from "../components/PageContent";
 
 function ErrorPage() {
   const error = useRouteError();
-  let title = "An error occured";
-  let message = "Something went wrong";
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
 
   if (error.status === 500) {
-    message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
+
   if (error.status === 404) {
-    title = "Not found";
-    message = "Could not find resource or page";
+    title = "Not found!";
+    message = "Could not find resource or page.";
   }
 
   return (
