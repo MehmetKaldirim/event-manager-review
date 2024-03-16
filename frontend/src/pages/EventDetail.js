a2d5a9c1b86b44c2beb0d3d43262725e50d12205
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-16 22:49:25 +0100

    delete work properly with useSubmit hook from router

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData, Link, json } from "react-router-dom";
+import { useRouteLoaderData, Link, json, redirect } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
@@ -30,4 +30,19 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({params,request}){
+  const id = params.eventId;
+
+  const response = await fetch('http://localhost:8080/events/' + id, {
+      method: request.method
+  })
+
+  if(!response.ok){
+      throw json({message:'Could not delete  event.'},{status:500})
+  } else {
+      return redirect('/events');
+  }
+
 }
